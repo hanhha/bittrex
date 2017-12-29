60a457ce6b38107b10ab6dc87ab0bd757af64589
Hanh Ha <haminhtranhanh@gmail.com>
2017-12-29 12:38:01 +0700

    Adding URL to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
-setup (name = 'bittrex', version = '1.0', py_modules=['bittrex'],
+setup (name = 'bittrex',
+			version = '1.0',
+			py_modules=['bittrex'],
       description = 'A Python wrapper for Bittrex exchange API',
       author = 'Hanh Ha',
       author_email = 'tranhanh.haminh@gmail.com',
-      license='MIT',
-      install_requires=[
+      license = 'MIT',
+			url = 'https://github.com/hanhha/bittrex',
+      install_requires = [
         'requests',
         'retrying',
         'simplejson',
